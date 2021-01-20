347bf7c59c97acb040888dc1e237ec3777b1b189
nicolanzi1 <nicolanzi@gmail.com>
2021-01-20 18:43:03 +0000

    Wrote the Hero component and elements, styled them with styled components, passed it into index.js, and added an assets/videos folder to import the video I used for the Hero component

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { HeroBg, HeroContainer, HeroContent, HeroH1, HeroItems, HeroP, VideoBg } from './HeroElements'
+import { Button } from '../Button'
+import Video from '../../assets/videos/travel.mp4'
+
+const Hero = () => {
+    return (
+        <HeroContainer>
+            <HeroBg>
+                <VideoBg src={Video} type="video/mp4" autoPlay loop muted playsInline />
+            </HeroBg>
+            <HeroContent>
+                <HeroItems>
+                    <HeroH1>Unreal Destinations</HeroH1>
+                    <HeroP>Out of this world</HeroP>
+                    <Button primary="true" big="true" round="true" to="/trips">Travel Now</Button>
+                </HeroItems>
+            </HeroContent>
+        </HeroContainer>
+    )
+}
+
+export default Hero
